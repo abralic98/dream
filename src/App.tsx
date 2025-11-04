e687efeb649bd92ab47c174234d717fecdb7c7b6
abralic98 <twogamers1950@gmail.com>
2025-11-04 19:11:41 +0100

    navigate to login if empty

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { ProtectedRoute } from "./features/auth/ProtectedRoute";
 import DashboardPage from "./pages/app/dashboard/DashboardPage";
 import ProfilePage from "./pages/app/profile/ProfilePage";
@@ -10,6 +10,7 @@ import GamePage from "./pages/app/dashboard/game/[id]/GamePage";
 const App = () => {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/auth/login" replace />} />
       <Route path="/auth/login" element={<LoginPage />} />
       <Route path="/auth/register" element={<RegisterPage />} />
       <Route path="*" element={<UnknownRoute />} />
